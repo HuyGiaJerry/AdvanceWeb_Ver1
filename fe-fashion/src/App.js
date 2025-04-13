efe4faad5a5d92fd168b53ee53cc5283674ece4f
HuyGiaJerry <huyfin2004@gmail.com>
2025-04-14 00:30:42 +0700

    Done Page NewArrival, cpn Footer

--- fe-fashion/src/App.js
+++ fe-fashion/src/App.js
@@ -1,12 +1,20 @@
 import './App.scss';
 import Header from './components/user/header/Header';
+import Footer from './components/user/footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UltimateSale from './components/user/ultimate-sale/Ultimate-sale';
+import NewArrival from './pages/user/newArrival';
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        
+        <div className='main-content'>
+          <UltimateSale />
+          <NewArrival />
+        </div>
+
+        <Footer />
       </Router>
     </div>
   );
--- fe-fashion/src/components/user/header/Header.js
+++ fe-fashion/src/components/user/header/Header.js
@@ -3,71 +3,73 @@ import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout } from '../../../store/store';
 import '../../../assets/fonts/Volkhov.scss';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
 const Header = () => {
     const dispatch = useDispatch();
     const isLoggin = useSelector((state) => state.auth.isLoggin);
     const [showFilter, setShowFilter] = useState(false);
     return (
-        <Navbar bg="light" expand="lg" className="header-container">
-            <Container>
-                <Navbar.Brand as={Link} to='/' className='navbar-brand'>FASCO</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={NavLink} to='/home' >Home</Nav.Link>
-                        {isLoggin ? (
-                            <>
-                                <Nav.Link as={NavLink} to='/shop'  >Shop</Nav.Link>
-                                <Nav.Link as={NavLink} to='/products' >Products</Nav.Link>
-                                <Nav.Link as={NavLink} to='/pages'>Pages</Nav.Link>
-                            </>
-                        ) : (
-                            <>
-                                <Nav.Link as={NavLink} to='/deals'>Deals</Nav.Link>
-                                <Nav.Link as={NavLink} to='/new-arrivals'>New Arrivals</Nav.Link>
-                                <Nav.Link as={NavLink} to='/packages'>Packages</Nav.Link>
-                            </>
-                        )}
-                    </Nav>
-                    <Nav className="ml-auto">
-                        {isLoggin ? (
-                            <>
+        <div className='header'>
+            <Navbar bg="white" expand="md" className="header-container">
+                <Container>
+                    <NavLink to='/' className='navbar-brand'>FASCO</NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink to='/home' className='nav-link' >Home</NavLink>
+                            {isLoggin ? (
+                                <>
+                                    <NavLink to='/shop' className='nav-link'>Shop</NavLink>
+                                    <NavLink to='/products' className='nav-link'>Products</NavLink>
+                                    <NavLink to='/pages' className='nav-link'>Pages</NavLink>
+                                </>
+                            ) : (
+                                <>
+                                    <NavLink to='/deals' className='nav-link'>Deals</NavLink>
+                                    <NavLink to='/new-arrivals' className='nav-link'>New Arrivals</NavLink>
+                                    <NavLink to='/packages' className='nav-link'>Packages</NavLink>
+                                </>
+                            )}
+                        </Nav>
+                        <Nav className="ml-auto">
+                            {isLoggin ? (
+                                <>
 
-                                {showFilter && (
-                                    <input
-                                        type="text"
-                                        className="filter-input"
-                                        placeholder="Search..."
-                                        autoFocus
-                                    />
-                                )}
-                                <Nav.Link as="button" onClick={() => setShowFilter(!showFilter)}>
-                                    <img src={require('../../../assets/icons/search.png')} alt="Search" />
-                                </Nav.Link>
-                                <Nav.Link as={NavLink} to='/profile' >
-                                    <img src={require('../../../assets/icons/user.png')} /></Nav.Link>
-                                <Nav.Link as={NavLink} to='/wish-list' >
-                                    <img src={require('../../../assets/icons/star.png')} /></Nav.Link>
-                                <Nav.Link as={NavLink} to='/cart' >
-                                    <img src={require('../../../assets/icons/cart.png')} /></Nav.Link>
-                                <Nav.Link as={NavLink} to='/home' onClick={() => dispatch(logout())} style={{ fontSize: '12px' }}>
-                                    Sign Out
-                                </Nav.Link>
-                            </>
-                        ) : (
-                            <>
-                                <Nav.Link as={NavLink} to='/home' style={{ fontSize: '12px' }} onClick={() => dispatch(login())}>Sign In</Nav.Link>
-                                <Button variant="dark" >
-                                    Sign Up
-                                </Button>
-                            </>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                                    {showFilter && (
+                                        <input
+                                            type="text"
+                                            className="filter-input"
+                                            placeholder="Search..."
+                                            autoFocus
+                                        />
+                                    )}
+                                    <NavLink as="button" onClick={() => setShowFilter(!showFilter)} className='nav-link'>
+                                        <img src={require('../../../assets/icons/search.png')} alt="Search" />
+                                    </NavLink>
+                                    <NavLink className='nav-link' to='/profile' >
+                                        <img src={require('../../../assets/icons/user.png')} /></NavLink>
+                                    <NavLink className='nav-link' to='/wish-list' >
+                                        <img src={require('../../../assets/icons/star.png')} /></NavLink>
+                                    <NavLink className='nav-link' to='/cart' >
+                                        <img src={require('../../../assets/icons/cart.png')} /></NavLink>
+                                    <NavLink className='nav-link' to='/home' onClick={() => dispatch(logout())} style={{ fontSize: '12px' }}>
+                                        Sign Out
+                                    </NavLink>
+                                </>
+                            ) : (
+                                <>
+                                    <NavLink className='nav-link' to='/home' style={{ fontSize: '12px' }} onClick={() => dispatch(login())}>Sign In</NavLink>
+                                    <Button variant="dark" >
+                                        Sign Up
+                                    </Button>
+                                </>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     );
 };
 
