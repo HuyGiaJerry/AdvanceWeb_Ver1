775ecabccdf0e929b29c57b77ace5ee81d8b6935
HuyGiaJerry <huyfin2004@gmail.com>
2025-04-17 13:24:52 +0700

    Done page : Login , signup,change pass

--- fe-fashion/src/App.js
+++ fe-fashion/src/App.js
@@ -5,6 +5,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UltimateSale from './components/user/ultimate-sale/Ultimate-sale';
 import NewArrival from './pages/user/newArrival';
 import FeedBack from './components/user/slider/FeedBack';
+import Login from './pages/user/login';
+import SignUp from './pages/user/signup';
+import ForgotPassWord from './pages/user/forgot_password';
+import ChangePassWord from './pages/user/new_password';
 function App() {
   return (
     <div className="App">
@@ -14,6 +18,10 @@ function App() {
           <UltimateSale />
           <NewArrival />
           <FeedBack />
+          <Login />
+          <SignUp />
+          <ForgotPassWord />
+          <ChangePassWord />
         </div>
 
         <Footer />
