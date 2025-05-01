6a68da77783ec69db189ae9a7bc1e4330a4f6eb0
HuyGiaJerry <huyfin2004@gmail.com>
2025-05-01 23:01:52 +0700

    Merge fe - App.js , Fix react-router-dom 18.3.1 -> 18.2.0

--- fe-fashion/src/App.js
+++ fe-fashion/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/user/header/Header';
@@ -14,19 +14,19 @@ import UserRoutes from './routes/UserRoutes';
 const AppContent = () => {
   const location = useLocation();
   const isAdminRoute = location.pathname.startsWith('/admin');
-  
+
   return (
     <>
       {/* Hiển thị Header chỉ khi không phải là trang admin */}
       {!isAdminRoute && <Header />}
-      
+
       <div className={!isAdminRoute ? 'main-content' : ''}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/shop" element={<MainShop />} />
           <Route path="/shop/product/detail/:id" element={<Detail />} />
-          
+
           {/* Tất cả các route admin được định nghĩa trong AdminRoutes */}
           <Route path="/admin/*" element={<AdminRoutes />} />
         </Routes>
