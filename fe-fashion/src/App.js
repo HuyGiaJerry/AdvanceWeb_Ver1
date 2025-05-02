d222818af2f09467142f53239cb840ae632ed17e
HuyGiaJerry <huyfin2004@gmail.com>
2025-05-02 15:26:36 +0700

    Fix route header

--- fe-fashion/src/App.js
+++ fe-fashion/src/App.js
@@ -1,49 +1,19 @@
 import React from 'react';
-
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Header from './components/user/header/Header';
-import MainShop from './pages/user/main_shop';
-import Home from './pages/user/home';
-import Detail from './pages/user/detail';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminRoutes from './routes/AdminRoutes';
+import UserRoutes from './routes/UserRoutes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-import UserRoutes from './routes/UserRoutes';
-// Kiểm tra đường dẫn và chỉ hiển thị Header cho các route không phải admin
-const AppContent = () => {
-  const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith('/admin');
-
-  return (
-    <>
-      {/* Hiển thị Header chỉ khi không phải là trang admin */}
-      {!isAdminRoute && <Header />}
-
-      <div className={!isAdminRoute ? 'main-content' : ''}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/shop" element={<MainShop />} />
-          <Route path="/shop/product/detail/:id" element={<Detail />} />
-
-          {/* Tất cả các route admin được định nghĩa trong AdminRoutes */}
-          <Route path="/admin/*" element={<AdminRoutes />} />
-        </Routes>
-      </div>
-    </>
-  );
-};
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <AppContent />
-
-
         <Routes>
+          {/* Các route người dùng */}
           <Route path="/*" element={<UserRoutes />} />
+
+          {/* Các route admin */}
           <Route path="/admin/*" element={<AdminRoutes />} />
         </Routes>
       </Router>
