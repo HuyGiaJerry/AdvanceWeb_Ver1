027f0da4d1d731ced3bba783d4cfb629b7dca39d
Hoang Trung Hieu <0983814204a@gmail.com>
2025-05-10 17:05:33 +0700

    api all color, size

---
+++ BE-Fashion/BE-Fashion/DTOs/AllColorDto.cs
@@ -0,0 +1,7 @@
+﻿namespace BE_Fashion.DTOs
+{
+    public class AllColorDto
+    {
+        public string ColorName { get; set; } = string.Empty;
+    }
+}
--- BE-Fashion/BE-Fashion/Program.cs
+++ BE-Fashion/BE-Fashion/Program.cs
@@ -63,10 +63,12 @@ builder.Services.AddLogging();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IRedisCartService, RedisCartService>();
 //builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<IProductColorRepository, ProductColorRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+builder.Services.AddScoped<IProductColorService, ProductColorService>();
+builder.Services.AddScoped<IRedisCartService, RedisCartService>();
 builder.Services.AddAutoMapper(typeof(ProductProfile));
 
 
